fb0972e8ac83073a8688037cf8560b3370c51628
LANA BANANA <lanabanana@Cristians-MacBook-Pro.local>
2026-06-16 15:18:55 -0300

    primeiro exercicio - controle estoque padaria

---
+++ ControleEstoquePadaria.java
@@ -0,0 +1,11 @@
+
+public class ControleEstoquePadaria {
+     public static void main(String[] args) {
+       var quantidadeFarinha = 10;
+       var quantidadeAcucar = 5;
+
+       System.out.println("A quantidade de farinha é" + quantidadeFarinha);
+       System.out.println("A quantidade de acucar é" + quantidadeAcucar);
+ }
+}
+
