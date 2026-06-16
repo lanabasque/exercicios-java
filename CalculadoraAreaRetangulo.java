b854b070579e74b978d6c14486a800cae4792d01
LANA BANANA <lanabanana@Cristians-MacBook-Pro.local>
2026-06-16 17:05:16 -0300

    exercicio calculadora area retangulo

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,13 @@
+
+
+  public class CalculadoraAreaRetangulo {
+     public static void main(String[] args) {
+       var largura = 5.0;
+       var altura = 3.0;
+       double area = largura * altura;
+
+
+       System.out.println( "Area = " + area);
+  
+ }
+}
