6f717d313783d7adbec19d4ab5e2dc77e81d6aba
LANA BANANA <lanabanana@Cristians-MacBook-Pro.local>
2026-06-22 12:51:37 -0300

    operadores atribuição exercício

---
+++ OperadoresAtribuicaoExercicio.java
@@ -0,0 +1,16 @@
+
+
+public class OperadoresAtribuicaoExercicio {
+     public static void main(String[] args) {
+       var numero = 10;
+       int numero1 = numero + 3;
+       int numero2 = numero - 2;
+       int numero3 = numero * 4;
+       int numero4 = numero / 2;
+      
+      System.out.println("Resultado adicao =  " + numero1);
+      System.out.println("Resultado subtracao =  " + numero2);
+      System.out.println("Resultado multiplicacao =  " + numero3);
+      System.out.println("Resultado divisao =  " + numero4);
+ }
+}
